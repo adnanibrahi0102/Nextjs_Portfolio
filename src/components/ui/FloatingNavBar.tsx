7935cac3c8b6af309db06d7ead16de800c9d7f32
Adnan ibrahim <adnanibrahi96@gmail.com>
2024-05-21 23:50:27 +0530

    fixed navbar

--- src/components/ui/FloatingNavBar.tsx
+++ src/components/ui/FloatingNavBar.tsx
@@ -68,8 +68,8 @@ export const FloatingNav = ({
               "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
             )}
           >
-            <span className="block sm:hidden">{navItem.icon}</span>
-            <span className="hidden sm:block text-sm">{navItem.name}</span>
+            {/* <span className="block ">{navItem.icon}</span> */}
+            <span className=" sm:block text-sm">{navItem.name}</span>
           </Link>
         ))}
        
