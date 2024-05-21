427f0989ec49c1a123b95cede0d1959c40ad91ca
Adnan ibrahim <adnanibrahi96@gmail.com>
2024-05-21 22:54:53 +0530

    update hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -20,31 +20,28 @@ const Hero = () => {
           fill="white"
         />
       </div>
-      <div className="h-screen w-full dark:bg-black bg-white  dark:bg-grid-white/[0.02] bg-grid-black/[0.2]  flex items-center justify-center absolute top-0 left-0">
+      <div className="h-screen w-full dark:bg-black bg-white dark:bg-grid-white/[0.02] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
         {/* Radial gradient for the container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-       
       </div>
 
-       <div className="flex justify-center relative my-20 z-10">
-           <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-              <h1 className="uppercase tracking-widest text-center text-blue-200 max-w-80">Eat Sleep code repeat</h1>
-              <TextGenerateEffect className="text-center text-[40px] md:text-6xl lg:text-8xl " words="Tranforming idea's into Seamless User Experiences"/>
-              <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-xl">Hi, I'm Adnan Ibrahim a Software Developer based in Kashmir</p>
-             <div className="flex flex-row gap-2">
-             <a href="#about">
-                <MagicButton title=" My Work"/>
-              </a>
-              <a href="">
-                <MagicButton title="Download CV"  />
-              </a>
-             </div>
-           </div>
-       </div>
+      <div className="flex justify-center relative my-20 z-10">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <h1 className="uppercase tracking-widest text-center text-blue-200 max-w-80">Eat Sleep code repeat</h1>
+          <TextGenerateEffect className="text-center text-[40px] md:text-6xl lg:text-8xl" words="Transforming idea&apos;s into Seamless User Experiences" />
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-xl">Hi, I'm Adnan Ibrahim, a Software Developer based in Kashmir</p>
+          <div className="flex flex-row gap-2">
+            <a href="#about">
+              <MagicButton title="My Work" />
+            </a>
+            <a href="">
+              <MagicButton title="Download CV" />
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Hero;
-
-
